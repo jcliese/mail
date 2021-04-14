584d58b4432f712102c45a35335c95b462c48c2d
jonathan <jonathan.liese@web.de>
2021-04-14 13:12:20 +0200

    init models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Listings")
+    title = models.CharField(max_length=256)
+    description = models.CharField(max_length=1024)
+    category = models.CharField(max_length=64)
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="Bids")
+    price = models.FloatField()
+    bid_date = models.DateField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="Comments")
+    comment = models.CharField(max_length=2048)
