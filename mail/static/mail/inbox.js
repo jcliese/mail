f83573b411dfeaaceea727725302cc9899110abf
jonathan <jonathan.liese@web.de>
2021-06-14 11:20:42 +0200

    send mails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -16,6 +16,50 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  // get user input
+  const submit = document.querySelector('input[type="submit"]');
+  const mailRecipient = document.querySelector('#compose-recipients');
+  submit.disabled = true;
+
+  mailRecipient.onkeyup = () => {
+    if (mailRecipient.value.length > 0) {
+        submit.disabled = false;
+    }
+    else {
+        submit.disabled = true;
+    }
+  }
+
+  document.querySelector('form').onsubmit = () => {
+    const recipients = mailRecipient.value.split(',');
+    const subject = document.querySelector('#compose-subject');
+    const body = document.querySelector('#compose-body');
+
+    recipients.forEach(recipient => {
+      try {
+        fetch('/emails', {
+          method: 'POST',
+          body: JSON.stringify({
+              recipients: recipient,
+              subject: subject,
+              body: body
+          })
+        })
+        .then(response => response.json())
+        .then(result => {
+            // Print result
+            console.log(result);
+        })
+      } catch (error){
+          console.log(error)
+        }
+        
+    })
+    submit.disabled = true;
+    load_mailbox('sent')
+    return false;
+  }
+
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
