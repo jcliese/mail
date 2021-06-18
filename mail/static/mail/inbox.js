4765db325d713b78ac4c293b89555e91c6c93f39
jonathan <jonathan.liese@web.de>
2021-06-18 12:41:38 +0200

    rm console.log

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -166,7 +166,6 @@ function load_detail(email, mailbox){
       detail.appendChild(hr);
       const body = document.createElement("div");
       body.style.whiteSpace = 'pre-wrap';
-      console.log(email.body)
       const body_text = document.createTextNode(email.body);
       body.appendChild(body_text);
       detail.appendChild(body);
